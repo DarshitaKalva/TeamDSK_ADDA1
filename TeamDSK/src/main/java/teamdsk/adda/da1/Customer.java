1db2de8bc96e03c798da1490ce04797702b6dbdb
DarshitaKalva <151772451+DarshitaKalva@users.noreply.github.com>
2024-11-12 15:30:27 +0530

    Update Customer.java

--- TeamDSK/src/main/java/teamdsk/adda/da1/Customer.java
+++ TeamDSK/src/main/java/teamdsk/adda/da1/Customer.java
@@ -9,5 +9,22 @@ package teamdsk.adda.da1;
  * @author darsh
  */
 public class Customer {
-    
+    private String name;
+    private int phoneNumber;
+    private String emailId;
+    private String userId;
+
+    public Customer(String name, int phoneNumber, String emailId, String userId) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        this.userId = userId;
+    }
+
+    public void updateInformation(String name, int phoneNumber, String emailId) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+        System.out.println("Customer information updated.");
+    }
 }
