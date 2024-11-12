af77f93f69ad2ba7d92e598fc4242d41633a0985
JayaSahithi06 <157402346+JayaSahithi06@users.noreply.github.com>
2024-11-12 15:26:25 +0530

    Update Account.java

--- TeamDSK/src/main/java/teamdsk/adda/da1/Account.java
+++ TeamDSK/src/main/java/teamdsk/adda/da1/Account.java
@@ -9,5 +9,37 @@ package teamdsk.adda.da1;
  * @author MADDU SAHITHI
  */
 public class Account {
+    private String accountNumber;
+    private String bankName;
+    private int balance;
+
+    public Account(String accountNumber, String bankName, int balance) {
+        this.accountNumber = accountNumber;
+        this.bankName = bankName;
+        this.balance = balance;
+    }
+
+    public int cashWithdrawal(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. Amount: " + amount);
+            return amount;
+        } else {
+            System.out.println("Insufficient balance.");
+            return 0;
+        }
+    }
+
+    public void cashDeposit(int amount) {
+        balance += amount;
+        System.out.println("Deposit successful. Amount: " + amount);
+    }
+
+    public String miniStatement() {
+        return "Account Number: " + accountNumber + ", Balance: " + balance;
+    }
     
+    public int getBalance() {
+        return balance;
+    }
 }
