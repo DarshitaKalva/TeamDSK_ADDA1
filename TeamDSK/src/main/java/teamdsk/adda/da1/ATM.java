be415d144b12130bf5e8a1754d140090807ccf8c
DarshitaKalva <151772451+DarshitaKalva@users.noreply.github.com>
2024-11-12 15:25:12 +0530

    Update ATM.java

--- TeamDSK/src/main/java/teamdsk/adda/da1/ATM.java
+++ TeamDSK/src/main/java/teamdsk/adda/da1/ATM.java
@@ -9,5 +9,26 @@ package teamdsk.adda.da1;
  * @author darsh
  */
 public class ATM {
-    
+    private int pin;
+    private String location;
+    private String bankName;
+
+    public ATM(String location, String bankName) {
+        this.location = location;
+        this.bankName = bankName;
+    }
+
+    public boolean validatePin(int pin) {
+        this.pin = pin;
+        // Dummy validation for demonstration
+        return true;
+    }
+
+    public void selectLanguage(String language) {
+        System.out.println("Language selected: " + language);
+    }
+
+    public void displayOptions() {
+        System.out.println("Options: Withdraw, Deposit, Mini Statement");
+    }
 }
