88f8c0e228923b91ebb2d2ab5ac3dfc848a74ac4
JayaSahithi06 <157402346+JayaSahithi06@users.noreply.github.com>
2024-11-12 15:27:21 +0530

    Update Transaction.java

--- TeamDSK/src/main/java/teamdsk/adda/da1/Transaction.java
+++ TeamDSK/src/main/java/teamdsk/adda/da1/Transaction.java
@@ -8,6 +8,23 @@ package teamdsk.adda.da1;
  *
  * @author MADDU SAHITHI
  */
+import java.util.Date;
+
 public class Transaction {
-    
+    private Date date;
+    private String type;
+    private int amount;
+
+    public Transaction(Date date, String type, int amount) {
+        this.date = date;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public void printReceipt() {
+        System.out.println("Transaction Receipt: ");
+        System.out.println("Date: " + date);
+        System.out.println("Type: " + type);
+        System.out.println("Amount: " + amount);
+    }
 }
